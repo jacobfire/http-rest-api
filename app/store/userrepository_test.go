2752f4d192687b19ee51e194f02f1a7a91a6a267
Jacob F <jacobfiregorilla@gmail.com.com>
2021-09-17 22:23:00 +0300

    testing DB

---
+++ app/store/userrepository_test.go
@@ -0,0 +1,38 @@
+package store_test
+
+import (
+	"github.com/jacobfire/http-rest-api/app/model"
+	"github.com/jacobfire/http-rest-api/app/store"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestUserRepository_Create(t *testing.T) {
+	s, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users")
+
+	u, err := s.User().Create(&model.User{
+		Email: "jack@gmail.com",
+	})
+
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
+
+func TestUserRepository_FindByEmail(t *testing.T) {
+	s, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users")
+
+	email := "test@test.com"
+
+	_, err := s.User().FindByEmail(email)
+	assert.Error(t, err)
+
+	s.User().Create(&model.User {
+		Email: "test@test.com",
+	})
+
+	u, err := s.User().FindByEmail(email)
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
